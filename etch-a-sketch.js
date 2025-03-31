4a22068e1ccf6a5aa5c05cde71f80f4c39b54645
Sekaii22 <shawn.ng22@gmail.com>
2025-04-01 01:25:22 +0800

    Add grid cell color change on hover

--- etch-a-sketch.js
+++ etch-a-sketch.js
@@ -1,3 +1,7 @@
+function changeBackgroundColor(e) {
+    e.target.style.backgroundColor = `black`;
+}
+
 function generateSketch(containerNode, noPerSides, cellLength) {
     for (let row = 1; row <= noPerSides; row++) {
         const rowContainer = document.createElement('div');
@@ -8,6 +12,7 @@ function generateSketch(containerNode, noPerSides, cellLength) {
         for (let col = 1; col <= noPerSides; col++) {
             let cell = document.createElement('div');
             cell.style.cssText = `border: 1px solid rgb(0, 0, 0, 0.1); height: ${cellLength}px; width: ${cellLength}px;`;
+            cell.addEventListener('mouseenter', changeBackgroundColor);
             rowContainer.appendChild(cell);
         }
     }
