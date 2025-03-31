dad56a56097a4ebc551d457ec60bbea6dce3415d
Sekaii22 <shawn.ng22@gmail.com>
2025-04-01 01:28:37 +0800

    Add grid generation using user input with input validation and error message

--- etch-a-sketch.js
+++ etch-a-sketch.js
@@ -19,8 +19,40 @@ function generateSketch(containerNode, noPerSides, cellLength) {
 }
 
 const container = document.querySelector('#container');
+const btn = document.querySelector('button');
+const errorSpan = document.querySelector('span');
 let cellsPerSide = 16;          // rows = cols = 16
 let length = 480;               // in pixels
 let cellLength = length / cellsPerSide;
 
+// by default, generates a 16x16 grid
 generateSketch(container, cellsPerSide, cellLength);
+
+btn.addEventListener('click', () => {
+    let userInput = prompt("Enter the number of squares per side (min 1, max 100):", '16');
+
+    // cancelled prompt or empty string
+    if (!userInput) return;
+
+    userInput = parseInt(userInput)
+
+    if (isNaN(userInput)) {
+        errorSpan.textContent = 'Error: non-numeric value was entered, try again.';
+    } 
+    else if (userInput > 100  || userInput <= 0) {
+        errorSpan.textContent = 'Error: value given was not within the range: [1, 100].';
+    }
+    else {
+        errorSpan.textContent = '';
+        cellsPerSide = userInput;
+        cellLength = length / cellsPerSide;
+
+        // delete grid
+        rowNodeList = document.querySelectorAll('.row');
+        rowNodeList.forEach((row) => container.removeChild(row));
+
+        // generate new grid
+        generateSketch(container, cellsPerSide, cellLength);
+    }
+
+})
