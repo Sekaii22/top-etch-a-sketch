63b7a104746127fc7a03b353f99c2e17fe80f875
Sekaii22 <shawn.ng22@gmail.com>
2025-04-01 02:33:55 +0800

    Enable draw mode only when holding down mouse button and moving mouse into grid cells

--- etch-a-sketch.js
+++ etch-a-sketch.js
@@ -1,5 +1,5 @@
 function changeBackgroundColor(e) {
-    const r = Math.floor(Math.random() * 256); // 0 to 255
+    const r = Math.floor(Math.random() * 256);       // 0 to 255
     const g = Math.floor(Math.random() * 256);
     const b = Math.floor(Math.random() * 256);
     e.target.style.backgroundColor = `rgb(${r}, ${g}, ${b})`;
@@ -9,25 +9,32 @@ function increaseOpacity(e) {
     const currentOpacity = e.target.style.opacity;
     
     if (!currentOpacity) {
-        e.target.style.opacity = 0.1;
+        e.target.style.opacity = 0.2;
     }
     else {
-        e.target.style.opacity = Math.min(parseFloat(currentOpacity) + 0.1, 1);
+        e.target.style.opacity = Math.min(parseFloat(currentOpacity) + 0.2, 1);
     }
 }
 
-function generateSketch(containerNode, noPerSides, cellLength) {
-    for (let row = 1; row <= noPerSides; row++) {
+function generateSketch(containerNode, ops) {
+    for (let row = 1; row <= ops.cellsPerSide; row++) {
         const rowContainer = document.createElement('div');
         rowContainer.classList.add('row');
         rowContainer.style.display = 'flex';
         containerNode.appendChild(rowContainer);
     
-        for (let col = 1; col <= noPerSides; col++) {
+        for (let col = 1; col <= ops.cellsPerSide; col++) {
             let cell = document.createElement('div');
-            cell.style.cssText = `border: 1px solid rgb(0, 0, 0, 0.1); height: ${cellLength}px; width: ${cellLength}px;`;
-            cell.addEventListener('mouseenter', changeBackgroundColor);
-            cell.addEventListener('mouseenter', increaseOpacity);
+            cell.style.cssText = `border: 1px solid rgb(0, 0, 0, 0.1); height: ${ops.cellLength}px; width: ${ops.cellLength}px;`;
+            
+            // add event listener to each cell
+            cell.addEventListener('mouseenter', (e) => {
+                if (ops.isDrawing) {
+                    changeBackgroundColor(e);
+                    increaseOpacity(e);
+                }
+            });
+
             rowContainer.appendChild(cell);
         }
     }
@@ -36,12 +43,20 @@ function generateSketch(containerNode, noPerSides, cellLength) {
 const container = document.querySelector('#container');
 const btn = document.querySelector('button');
 const errorSpan = document.querySelector('span');
-let cellsPerSide = 16;          // rows = cols = 16
-let length = 480;               // in pixels
-let cellLength = length / cellsPerSide;
+
+// using object for passing isDrawing by reference
+let options = {
+    cellsPerSide: 16,      // rows = cols = 16
+    length: 480,           // in pixels
+    isDrawing: false,
+}
+options.cellLength = options.length / options.cellsPerSide;
 
 // by default, generates a 16x16 grid
-generateSketch(container, cellsPerSide, cellLength);
+generateSketch(container, options);
+
+document.addEventListener('mousedown', () => options.isDrawing = true);
+document.addEventListener('mouseup', () => options.isDrawing = false);
 
 btn.addEventListener('click', () => {
     let userInput = prompt("Enter the number of squares per side (min 1, max 100):", '16');
@@ -59,15 +74,15 @@ btn.addEventListener('click', () => {
     }
     else {
         errorSpan.textContent = '';
-        cellsPerSide = userInput;
-        cellLength = length / cellsPerSide;
+        options.cellsPerSide = userInput;
+        options.cellLength = options.length / options.cellsPerSide;
 
         // delete grid
         rowNodeList = document.querySelectorAll('.row');
         rowNodeList.forEach((row) => container.removeChild(row));
 
         // generate new grid
-        generateSketch(container, cellsPerSide, cellLength);
+        generateSketch(container, options);
     }
 
 })
